33c8777f008c95562f74014a20cec6b2ab7d0328
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-09-04 00:43:19 +0200

    The render Props Pattern

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import { faker } from "@faker-js/faker";
+import List from "./List";
+import ProductItem from "./ProductItem";
+import CompanyItem from "./CompanyItem";
+
+const products = Array.from({ length: 20 }, () => {
+  return {
+    productName: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: faker.commerce.price()
+  };
+});
+
+const companies = Array.from({ length: 15 }, () => {
+  return {
+    companyName: faker.company.name(),
+    phrase: faker.company.catchPhrase()
+  };
+});
+
+export default function App() {
+  return (
+    <div>
+      <h1>Render Props Demo</h1>
+
+      <div className="col-2">
+        <List
+          title="Products"
+          items={products}
+          render={(product) => (
+            <ProductItem key={product.productName} product={product} />
+          )}
+        />
+
+        <List
+          title="Companies"
+          items={companies}
+          render={(company) => (
+            <CompanyItem
+              key={company.companyName}
+              company={company}
+              defaultVisibility={false}
+            />
+          )}
+        />
+      </div>
+    </div>
+  );
+}
